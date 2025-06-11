6f0323e7a3fce78631d37ac2deda4910aa67619f
Alireza78na <144834309+Alireza78na@users.noreply.github.com>
2025-06-11 14:46:36 +0330

    Refactor configuration system

--- telegram_filebot_full (1)/app/core/db.py
+++ telegram_filebot_full (1)/app/core/db.py
@@ -1,9 +1,9 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+from app.core.config import config
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(config.DATABASE_URL, echo=config.DATABASE_ECHO)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 Base = declarative_base()
